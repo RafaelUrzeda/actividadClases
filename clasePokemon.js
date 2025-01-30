4a70394426ce04a253e55a92c960cc005bdf5d72
dreig-dawi <dreigbuades@cifpfbmoll.eu>
2025-01-30 17:02:41 +0100

    index changes

--- clasePokemon.js
+++ clasePokemon.js
@@ -8,18 +8,18 @@ export class Pokemon {
         this.velocidad = velocidad;
     }
 
-    atacar(oponente) {
+    atack(oponente) {
         let daño = Math.max(5, this.ataque - oponente.defensa); 
         oponente.vida -= daño;
         console.log(`${this.nombre} ataca a ${oponente.nombre} causando ${daño} de daño.`);
     }
 
-    curar() {
+    heal() {
         this.vida += 20;
         console.log(`${this.nombre} se ha curado 20 puntos de vida.`);
     }
 
-    mostrarVida() {
+    showHealth() {
         console.log(`${this.nombre} tiene ${this.vida} puntos de vida.`);
     }
 }
